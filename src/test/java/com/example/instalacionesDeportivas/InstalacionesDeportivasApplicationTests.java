e339a2f405fa2443999dbdbf56806ff8442b71c3
Sofia Rebagliato <sofia.rebagliatto@gmail.com>
2021-09-13 10:39:30 +0200

    Prueba

---
+++ src/test/java/com/example/instalacionesDeportivas/InstalacionesDeportivasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.instalacionesDeportivas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstalacionesDeportivasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
