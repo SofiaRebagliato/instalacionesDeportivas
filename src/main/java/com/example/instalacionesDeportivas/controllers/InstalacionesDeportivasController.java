9c796170765a16451c783c503c158870dd3172e9
Manuel Lopez Lopez <72388229+ManuelLopezLopez@users.noreply.github.com>
2021-09-14 13:32:09 +0200

    VistaInicioEmpezada

--- src/main/java/com/example/instalacionesDeportivas/controllers/InstalacionesDeportivasController.java
+++ src/main/java/com/example/instalacionesDeportivas/controllers/InstalacionesDeportivasController.java
@@ -17,8 +17,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class InstalacionesDeportivasController {
     
 	@GetMapping("/index")
-	@ResponseBody
+	
 	public String getPrueba() {
-		return "index.html";
+		return "inicio/index.html";
 	}
 }
