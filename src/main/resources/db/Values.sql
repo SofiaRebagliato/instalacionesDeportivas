30954922c7a353476baf9cbcdc0246430624b716
Manuel Lopez Lopez <72388229+ManuelLopezLopez@users.noreply.github.com>
2021-09-15 12:48:09 +0200

    sql values

---
+++ src/main/resources/db/Values.sql
@@ -0,0 +1,8 @@
+
+ INSERT INTO instalaciones (IdInstalaciones, Nombre, Descripcion, Imagen, Precio) VALUES 
+ (null,'Pista de Baloncesto', 'Bonita pista', 'https://vermarconstrucciones.es/wp-content/uploads/2020/04/pista-de-tenis-hormigon.jpg', '200'),
+ (null,'Pista de volleyball', 'Bonita pista', 'https://vermarconstrucciones.es/wp-content/uploads/2020/04/pista-de-tenis-hormigon.jpg', '200'),
+ (null,'Pista de Tenis', 'Bonita pista', 'https://vermarconstrucciones.es/wp-content/uploads/2020/04/pista-de-tenis-hormigon.jpg', '200'),
+ (null,'Gimnasio', 'Bonita pista', 'https://vermarconstrucciones.es/wp-content/uploads/2020/04/pista-de-tenis-hormigon.jpg', '200');
+
+ 
