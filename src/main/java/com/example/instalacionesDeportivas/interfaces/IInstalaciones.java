24d571820408468042e8070d7513d094c71a85a2
Sofia Rebagliato <sofia.rebagliatto@gmail.com>
2021-09-15 10:27:42 +0200

    CRUD Instalaciones a falta de los html

--- src/main/java/com/example/instalacionesDeportivas/interfaces/IInstalaciones.java
+++ src/main/java/com/example/instalacionesDeportivas/interfaces/IInstalaciones.java
@@ -1,14 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.example.instalacionesDeportivas.interfaces;
 
-/**
- *
- * @author Sofia
- */
+import com.example.instalacionesDeportivas.entities.instalaciones;
+import java.util.List;
+
 public interface IInstalaciones {
     
+    /**
+     * Busca el ID de la instalación y si ya existe modifica el resto de campos (o lo sustituye)
+     * si no está el ID la inserta.
+     */
+     public void guardarInstalacion(instalaciones instalaciones);
+     
+     /**
+     * Borra la instalación del ID indicado.
+     */
+     public void borrarInstalacion(int IdInstalaciones);
+     
+     /**
+     * Devuelve todas las instalaciones.
+     */
+     public List<instalaciones> getInstalaciones();
 }
--- src/main/java/com/example/instalacionesDeportivas/services/InstalacionesService.java
+++ src/main/java/com/example/instalacionesDeportivas/services/InstalacionesService.java
@@ -4,6 +4,7 @@ import com.example.instalacionesDeportivas.entities.instalaciones;
 import com.example.instalacionesDeportivas.interfaces.IInstalaciones;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,9 +13,38 @@ public class InstalacionesService implements IInstalaciones{
     private List<instalaciones> listaInstalaciones = new ArrayList<>();
     
     public InstalacionesService() {
-        //listaInstalaciones.add(new instalaciones(0, "pista de tenis", "", "", "", ));
-        //listaAlumnos.add(new Alumno(2L, "Ana", "444", "C/rwr"));
-        //listaAlumnos.add(new Alumno(3L, "Pedro", "6753648", "C/pogbtr"));
-        //listaAlumnos.add(new Alumno(4L, "Lucas", "6543895", "C/hygdtfrb"));
+        //listaInstalaciones.add(new instalaciones(0, "Cancha de Baloncesto", "Fabulosa pista de baloncesto cubierta para poder disfrutar del deporte que más te gusta en cualquier estación del año", 'C:/Users/Sofia/Desktop/CursoJavaHiberus/instalacionesDeportivas/src/main/resources/static/images',  3.00));
+        //listaInstalaciones.add(new Alumno(0, "Voleibol", "Contamos con dos pistas de voleibal. Una cubierta y otra cerrada.", 3.00));
+        //listaInstalaciones.add(new Alumno(0, "Pista de tenis", "Magníficas pistas de tenis con variedad de superficies y características: tierra batida y resina, cubiertas y descubiertas; para alquilar independientemente de las condiciones climatológicas existentes.", 'C:/Users/Sofia/Desktop/CursoJavaHiberus/instalacionesDeportivas/src/main/resources/static/images',  3.00));
+        //listaInstalaciones.add(new Alumno(0, "Gimnasio", "La zona de gimnasio cuanta con una gran variedad de máquinas. Además contamos con un entrenador prsonal que te va a ayudar a conseguir tus objetivos", 3.00));
+    }
+
+    @Override
+    public void guardarInstalacion(instalaciones instalaciones) {
+        
+       Optional<instalaciones> guardarInstalacion = listaInstalaciones.stream()
+               .filter(instal -> instal.getIdInstalaciones() == instalaciones.getIdInstalaciones())
+               .findFirst();
+       
+        if (guardarInstalacion.isPresent()) {
+            guardarInstalacion.get().setIdInstalaciones(instalaciones.getIdInstalaciones());
+            guardarInstalacion.get().setNombre(instalaciones.getNombre());
+            guardarInstalacion.get().setImagen(instalaciones.getImagen());
+            guardarInstalacion.get().setDescripcion(instalaciones.getDescripcion());
+            guardarInstalacion.get().setPrecio(instalaciones.getPrecio());
+        } else {
+            instalaciones.setIdInstalaciones((int)(Math.random()*999999999));
+            listaInstalaciones.add(instalaciones);
+        }
+    }
+    
+    @Override
+    public void borrarInstalacion(int IdInstalaciones) {
+        listaInstalaciones.removeIf(instalaciones -> instalaciones.getIdInstalaciones() == IdInstalaciones);
+    }
+    
+    @Override
+    public List<instalaciones> getInstalaciones() {
+        return listaInstalaciones;
     }
 }
