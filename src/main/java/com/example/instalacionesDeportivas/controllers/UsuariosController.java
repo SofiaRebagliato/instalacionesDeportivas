6cb9d5e01ec06e135431fa535fdcf7193a54004e
Manuel Lopez Lopez <72388229+ManuelLopezLopez@users.noreply.github.com>
2021-10-01 14:58:16 +0200

    cositas guays

--- src/main/java/com/example/instalacionesDeportivas/controllers/UsuariosController.java
+++ src/main/java/com/example/instalacionesDeportivas/controllers/UsuariosController.java
@@ -53,7 +53,7 @@ public class UsuariosController {
         } else {
             return "redirect:verUsuarios";
         }
-        return "/usuarios/formularioUsuarios2";
+        return "/usuarios/formularioUsuarios";
     }
     
     @PostMapping("/guardarUsuarios")
